a97e3302701a3ce4d69baf67383c796035eb1343
Luca Camphuisen <luca@logic-shift.com>
2025-06-13 16:45:12 +0200

    remove all

--- src/templates/static.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-
-import AnchorHeading from 'components/shared/anchor-heading';
-
-const components = {
-  h2: AnchorHeading('h2'),
-  h3: AnchorHeading('h3'),
-};
